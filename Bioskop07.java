6604303fbfa67f1bfdfb69c06d2fbce18f1354b2
Geveriell <erzaardhyaputra@gmail.com>
2025-11-12 11:39:35 +0700

    modifikasi percobaan 1.6

--- Bioskop07.java
+++ Bioskop07.java
@@ -14,10 +14,10 @@ public class Bioskop07 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-        // Menampilkan penonton baris ke-3
-        System.out.println("\nPenonton baris ke-3:");
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        // Menampilkan penonton baris ke-3 dengan foreach
+        System.out.println("\nPenonton baris ke-3 (foreach):");
+        for (String nama : penonton[2]) {
+            System.out.println(nama);
         }
     }
 }
