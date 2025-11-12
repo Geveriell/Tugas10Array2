3c745166c01a017a70a54d0b910cced5d53e7123
Geveriell <erzaardhyaputra@gmail.com>
2025-11-12 12:09:08 +0700

    modifikasi percobaan 2.1

--- BioskopWithScanner07.java
+++ BioskopWithScanner07.java
@@ -3,37 +3,47 @@ import java.util.Scanner;
 public class BioskopWithScanner07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int baris, kolom;
-        String nama, next;
         String[][] penonton = new String[4][2];
+        int menu;
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-            sc.nextLine(); // membersihkan buffer
+        do {
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Daftar Penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
+            sc.nextLine(); // bersihkan buffer
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("Masukkan baris (1-4): ");
+                    int baris = sc.nextInt();
+                    System.out.print("Masukkan kolom (1-2): ");
+                    int kolom = sc.nextInt();
+                    sc.nextLine();
+                    penonton[baris - 1][kolom - 1] = nama;
+                    break;
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                case 2:
+                    System.out.println("\nDaftar Penonton:");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            System.out.print(penonton[i][j] + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case 3:
+                    System.out.println("Terima kasih!");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid!");
             }
-        }
-
-        System.out.println("\nDaftar Penonton:");
-        for (int i = 0; i < penonton.length; i++) {
-            for (int j = 0; j < penonton[i].length; j++) {
-                System.out.print(penonton[i][j] + "\t");
-            }
-            System.out.println();
-        }
+        } while (menu != 3);
 
         sc.close();
     }
