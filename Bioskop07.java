a899dfa7a14f7aae5a04dce1ef48482f2e247e6f
Geveriell <erzaardhyaputra@gmail.com>
2025-11-12 11:24:10 +0700

    modifikasi percobaan 1.2

--- Bioskop07.java
+++ Bioskop07.java
@@ -14,16 +14,6 @@ public class Bioskop07 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-        //modifikasi supaya tidak null
-        penonton[0][0] = "Amin";
-        penonton[0][1] = "Bena";
-        penonton[1][0] = "Candra";
-        penonton[1][1] = "Dela";
-        penonton[2][0] = "Eka";
-        penonton[2][1] = "Farhan";
-        penonton[3][0] = "Gisel";
-        penonton[3][1] = "Hana";
-
         // array for loop
         for (int i = 0; i < penonton.length; i++) {
             for (int j = 0; j < penonton[i].length; j++) {
@@ -31,5 +21,12 @@ public class Bioskop07 {
             }
             System.out.println();
         }
+
+        // Menampilkan panjang array
+        System.out.println("\nJumlah baris: " + penonton.length);
+        System.out.println("Jumlah kolom baris ke-0: " + penonton[0].length);
+        System.out.println("Jumlah kolom baris ke-1: " + penonton[1].length);
+        System.out.println("Jumlah kolom baris ke-2: " + penonton[2].length);
+        System.out.println("Jumlah kolom baris ke-3: " + penonton[3].length);
     }
 }
